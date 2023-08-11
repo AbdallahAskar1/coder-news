76a4baa211b915845d297a2c8ee9c9edbbd6cf9f
Abdallah Askar <abdoasker.1426@gmail.com>
2023-08-11 18:17:48 +0300

    add react-router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react";
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import PostPage from './pages/Post';
+import PostsPage from './pages/Posts';
 export default function App() {
-  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:4000/v1/posts").then((res) =>
-      res.json().then((data) => {
-        setPosts(data.posts);
-        console.log(posts)
-      })
-    );
-  },[]);
-  return (
-    <>
-      <h1>hi everyone {JSON.stringify(posts)} {posts.map(post=>post.title)}</h1>
-    </>
+
+  return ( 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostsPage/>}/>
+        <Route path="/post/:id" element={<PostPage/>}/>
+
+      </Routes> 
+    </BrowserRouter>
   );
 }
 
 // export default App
+ 
